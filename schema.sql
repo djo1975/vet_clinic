f01e9204e3301d835f0cbb0b32b0791b30109a59
djo1975 <mladanilic1975@gmail.com>
2023-04-04 11:32:22 +0200

    Initial commit

--- schema.sql
+++ schema.sql
@@ -1,11 +1,13 @@
 /* Database schema to keep the structure of entire database. */
 
-vet_clinic=# CREATE TABLE animals (
+CREATE TABLE animals (
   id INTEGER PRIMARY KEY DEFAULT nextval('animals_id_seq'),
   name VARCHAR(255),
   date_of_birth DATE,
   escape_attempts INTEGER,
   neutered BOOLEAN,
-  weight_kg DECIMAL(5,2)
+  weight_kg DECIMAL(5,2),
+  species VARCHAR(255)
 );
+
 
