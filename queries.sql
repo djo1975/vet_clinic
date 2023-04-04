be3628c8d238c7cc6c0beac65fea5d2a0ea8991a
djo1975 <mladanilic1975@gmail.com>
2023-04-04 12:49:19 +0200

    Added queries

--- queries.sql
+++ queries.sql
@@ -16,4 +16,53 @@ SELECT * FROM animals
 WHERE name <> 'Gabumon';
 SELECT * FROM animals
 WHERE weight_kg BETWEEN 10.4 AND 17.3;
+-- Update animals table to set species as unspecified and then roll back
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals WHERE species = 'unspecified';
+ROLLBACK;
+SELECT * FROM animals WHERE species = 'unspecified';
 
+-- Update animals table to set species as digimon and then set species as pokemon
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
+SELECT * FROM animals;
+
+-- Delete all records in the animals table and then roll back
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT my_savepoint;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO my_savepoint;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+SELECT * FROM animals;
+
+-- How many animals are there?
+SELECT COUNT(*) FROM animals;
+
+-- How many animals have never tried to escape?
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+-- What is the average weight of animals?
+SELECT AVG(weight_kg) FROM animals;
+
+-- Who escapes the most, neutered or not neutered animals?
+SELECT neutered, SUM(escape_attempts) AS total_escape_attempts
+FROM animals
+GROUP BY neutered
+ORDER BY total_escape_attempts DESC
+LIMIT 1;
+
+-- What is the minimum and maximum weight of each type of animal?
+SELECT species, MIN(weight_kg), MAX(weight_kg)
+FROM animals
+GROUP BY species;
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT species, AVG(escape_attempts) AS avg_escape_attempts
+FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
--- schema.sql
+++ schema.sql
@@ -1,13 +1,13 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
+vet_clinic=# CREATE TABLE animals (
   id INTEGER PRIMARY KEY DEFAULT nextval('animals_id_seq'),
   name VARCHAR(255),
   date_of_birth DATE,
   escape_attempts INTEGER,
   neutered BOOLEAN,
-  weight_kg DECIMAL(5,2),
-  species VARCHAR(255)
+  weight_kg DECIMAL(5,2)
 );
-
+vet_clinic=# ALTER TABLE animals ADD COLUMN species VARCHAR(255);
+ALTER TABLE
 
